c5138fdcff6d1e9de4ac5ff6d5e2adae6477c48d
iaschnei <iaschnei@student.42.fr>
2024-08-29 09:58:07 +0200

    Added coordinates updates for all aircrafts

--- src/Flyable/Balloon.java
+++ src/Flyable/Balloon.java
@@ -1,5 +1,7 @@
 package Flyable;
 
+import Tower.UnregisteredException;
+
 public class Balloon extends Aircraft {
 
   public Balloon(long p_id, String p_name, Coordinates p_coordinate) {
@@ -7,9 +9,54 @@ public class Balloon extends Aircraft {
   }
 
   @Override
-  public void updateConditions() {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException(
-        "Unimplemented method 'updateConditions'");
+  public void updateConditions()
+      throws NoTowerException, UnregisteredException {
+
+    if (this.weatherTower == null) {
+      throw new NoTowerException("This aircraft is not registered to a tower " +
+          "and cannot get weather info");
+    }
+
+    String weather = this.weatherTower.getWeather(this.coordinates);
+
+    switch (weather) {
+
+      case "RAIN":
+        this.coordinates.setHeight(this.coordinates.getHeight() - 5);
+        if (this.current_weather != weather) {
+          System.out.println(this.name + "(" + this.id + ") : Rain");
+          this.current_weather = weather;
+        }
+        break;
+
+      case "FOG":
+        this.coordinates.setHeight(this.coordinates.getHeight() - 3);
+        if (this.current_weather != weather) {
+          System.out.println(this.name + "(" + this.id + ") : Fog");
+          this.current_weather = weather;
+        }
+        break;
+
+      case "SUN":
+        this.coordinates.setLongitude(this.coordinates.getLongitude() + 2);
+        this.coordinates.setHeight(this.coordinates.getHeight() + 4);
+        if (this.current_weather != weather) {
+          System.out.println(this.name + "(" + this.id + ") : Sun");
+          this.current_weather = weather;
+        }
+        break;
+
+      case "SNOW":
+        this.coordinates.setHeight(this.coordinates.getHeight() - 15);
+        if (this.coordinates.getHeight() == 0) {
+          this.weatherTower.unregister(this);
+          return;
+        }
+        if (this.current_weather != weather) {
+          System.out.println(this.name + "(" + this.id + ") : Snow");
+          this.current_weather = weather;
+        }
+        break;
+    }
   }
 }
